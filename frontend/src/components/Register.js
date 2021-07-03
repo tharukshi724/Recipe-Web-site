abaff17588782019c7ecc512ed0457f923b394d7
tharukshi724 <tharukshiw@gmail.com>
2021-07-03 10:23:47 +0530

    modify Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 import back from './images/back.png';
 import register from './images/register.png';
 import profile from './images/profile.png';
 
 import { Link } from "react-router-dom";
 import  './Styles/register.css';
+import axios from 'axios';
 
 
 
 export default function Register() {
+     
+    const[firstname , setFirstname] = useState("");
+    const[lastname,setLastname] = useState("");
+    const[username,setUsername] = useState("");
+    const[email,setEmail] = useState("");
+    const[password,setPassword] = useState("");
+
+    function subData(e){
+         e.preventDefault();
+
+         const newUser={
+             firstname,
+             lastname,
+             username,
+             email,
+             password
+         }
+
+       axios.post("http://localhost:8070/user/adduser",newUser).then(()=>{
+           alert("success");
+       }).catch((err)=>{
+           alert(err);
+       })
+    }
+
+
     return (
         <div>
             <div className="fContainer">
@@ -27,22 +54,39 @@ export default function Register() {
                      
                  </div>
                  <div class="wrapper">
-                     <form className="ui big form">
+                     <form className="ui big form" onSubmit={subData}>
                          <div className="sixteen wide field">
                         <label>First name</label>
-                        <input type="text" name="fristname" placeholder="Enter First name"></input>
+                        <input type="text" name="fristname" placeholder="Enter First name" onChange={(e)=>{
+                            setFirstname(e.target.value);
+                        }}></input>
                          </div>
                          <div className="field">
                         <label>Last name</label>
-                       <input type="text" name="password" placeholder="Enter Last name"></input>
+                       <input type="text" name="lastname" placeholder="Enter Last name" onChange={(e)=>{
+                           setLastname(e.target.value);
+                       }}></input>
                          </div>
+
                          <div className="field">
+                         <label>Username</label>
+                             <input type="text" name="username" placeholder="Enter a username" onChange={(e)=>{
+                                 setUsername(e.target.value);
+                             }}></input>
+                             </div>
+                         <div className="field">
+                       
+
                         <label>Email</label>
-                       <input type="email" name="email" placeholder="Enter Email"></input>
+                       <input type="email" name="email" placeholder="Enter Email" onChange={(e)=>{
+                           setEmail(e.target.value);
+                       }}></input>
                          </div>
                          <div className="field">
                         <label>Password</label>
-                       <input type="password" name="password" placeholder="Enter password"></input>
+                       <input type="password" name="password" placeholder="Enter password" onChange={(e)=>{
+                           setPassword(e.target.value);
+                       }}></input>
                          </div>
                          <Link to="#" className="linkTo">Already SignUp</Link>
                         
