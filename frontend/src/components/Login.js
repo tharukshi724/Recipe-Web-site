f378976b2d7cf95afcec4163beed3c61284c423c
tharukshi724 <tharukshiw@gmail.com>
2021-07-03 10:23:13 +0530

    modify Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React,{useRef,useState,useEffect} from 'react';
 import back from './images/back.png';
 import loginimg from './images/loginimg.png';
 import profile from './images/profile.png';
 
 import { Link } from "react-router-dom";
 import  './Styles/login.css';
+import axios from 'axios';
 
 
 
 export default function Login() {
+
+    
+    const[username,setUsername] = useState("");
+    const[password,setPassword] = useState("");
+    const myInput = useRef();
+
+
+    useEffect(()=>
+         myInput.current && myInput.current.focus());
+    
+    function validate(e){
+
+        e.preventDefault();
+        const userValidate ={
+            username,
+            password
+        }
+       axios.post("http://localhost:8070/user/validate",userValidate).then(()=>{
+           alert("succeefully log in");
+       }).catch((err)=>{
+           alert(err);
+       })
+
+    }
+   
     return (
         <div>
             <div className="fContainer">
@@ -26,14 +52,18 @@ export default function Login() {
                      <h1>Welcome</h1>
                  </div>
                  <div class="wrapper">
-                     <form className="ui big form">
+                     <form className="ui big form" onSubmit={validate}>
                          <div className="sixteen wide field">
                         <label>Username</label>
-                        <input type="text" name="Username" placeholder="Enter Username"></input>
+                        <input type="text" name="Username" placeholder="Enter Username" inputRef={myInput} onChnage={(e)=>{
+                            setUsername(e.target.value);
+                        }}></input>
                          </div>
                          <div className="field">
                         <label>Password</label>
-                       <input type="password" name="password" placeholder="Enter password"></input>
+                       <input type="password" name="password" placeholder="Enter password" onChange={(e)=>{
+                           setPassword(e.target.value);
+                       }}></input>
                          </div>
                          <Link to="#" className="linkTo">Forget Password</Link>
                         
