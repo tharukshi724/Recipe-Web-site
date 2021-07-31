8ad9e9209045d59e8aa3e843a6fd6ddcbbd212a0
tharukshi724 <tharukshiw@gmail.com>
2021-07-31 07:56:41 +0530

    modify changes

--- frontend/src/components/Landingpage.js
+++ frontend/src/components/Landingpage.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import Cards from './Cards';
+import Description from './Description';
 
 import Footer from './Footer';
-import breakfast from './images/hands.jpg';
+import main from './images/main.jpg';
 import './Styles/Landingpage.css';
 
 function Landingpage() {
     return (
         <div>
-             <img src={breakfast}></img>
+             
            <div className="hero-container">
+           <img src={main}></img>
             
-             <h1>Wow Foods</h1>
-             <p>What are you waiting for ?</p>
-             <div class="hero-btn">
-                <button className="ui large button" buttonStyle="ui outline" buttonSize="ui large button">Get Started</button>
-
-            </div>  
-            <div class="hero-btn">
-                <button className="ui primary large button" buttonStyle="ui primary" buttonSize="ui primary large button">Watch</button>
-
-            </div>        
+            <div className="titleName">
+                <h1>Tasty Food</h1>
+            </div> 
+            <div className="dis">
+                <h3>Try Best Recipes All Over The World</h3>
+            </div>
+            <div className="btn">
+                <button type="submit" className="huge ui inverted green button">Get Started</button>
+                </div>
           </div> 
 
           
      
          
             <Cards></Cards>
-         
-            <Footer/>
+          
+           <Footer/>
+          
+          
         </div>
          
     )
